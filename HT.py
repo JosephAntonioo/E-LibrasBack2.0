f0bb1ce8234176a87dc54faef0dad1f7ced2e68d
José Favaro <jose-tof@hotmail.com>
2021-11-27 12:56:46 -0300

    Ajustes para retornar a descrição do gesto mobile

--- HT.py
+++ HT.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
-from modulos.alfabeto import A
+from modulos.alfabeto import letras
 
 sys.path.append('./modulos/')
 #sys.path
@@ -19,25 +19,25 @@ import extrator_ALTURA as altura
 import extrator_PROXIMIDADE as proximidade
 import alfabeto as alfabeto
 import cv2
+import time
 
 
 
-def srcPicture(img):
+async def srcPicture(img):
     # print('ok')
+    time.sleep(10)
     img = cv2.imread(img, flags=cv2.IMREAD_COLOR)
     # print(img)
     pontos = handTrack(img)
     verifDedos(pontos)
     verifProxi(pontos)
-    resultado = []
     resultado = leituraGesto(pontos)
     # print('Descrição do gesto :')
-    print(resultado)
+    # print(resultado)
     # print('Letra Index :')
     # # print(indexLetra)
     # str1 = ''.join(descricaoG)
-    print(alfabeto.letras[1])
-    finalS = resultado[1] #, resultado[0]
+    finalS = resultado #, resultado[0]
     #Agora precisa rodar um metodo para fazer os parangole de sc
     print(finalS)
     return finalS
@@ -199,12 +199,14 @@ def verifProxi(pontos):
     #  'anelar dobrado: proximo ao minimo',
     #  'minimo dobrado: proximo ao anelar']
     p = proximidade.verificar_proximidade_DEDOS(pontos)
+    print('proximidade:',p)
     return p
 
 def leituraGesto(pontos):
     # print('------------------------xx---------xx--------------------')
     # Faz o link com o array de array de alfabeto letras, que pode ser incrementado no futuro
     atual = proximidade.verificar_proximidade_DEDOS(pontos)
+    print('atual:', atual)
     # return atual
     for i, a in enumerate(alfabeto.letras):
         if(alfabeto.letras[i] == atual):
@@ -215,8 +217,169 @@ def leituraGesto(pontos):
             # print('Deu boa!!!!!!!!')
             final = []
             final.append(a)
+            print(a)
             final.append(i)
             # print('return é [a,i]')
-            return final
+    print(final[0])
+    print(final[1])
+    return final[1]
     print('Deu ruim')
     return 'deu em nada'
+
+async def final(img):
+    # print('ok')
+    img = cv2.imread(img, flags=cv2.IMREAD_COLOR)
+
+
+      #Modulo extrator:
+        #ALTURA: verifica se um ponto está acima ou abaixo de outro ponto
+        #POSICAO: funcoes para verificar se os dedos estao dobrados ou esticados, horizontal vertical
+        # tambem recebe o resultado do modulo altura para saber em que posicao a mao estao acima ou abaixo
+        #PROXIMIDADE: funcoes que compara a proximidade entre os pontos detectados, se o resultado de posicao
+        # for igual dobrado para o indicador e dedo medio e ambos estiverem na mesma altura entao eles estao proximos
+    
+    #ALFABETO***: apos extrair todas as caracteristicas foi melhorado o alfabeto de caracteristicas base,
+    # onde um vetor de vetores recebe o resultado de todos os modulos extratores. Assim usamos este modulo para
+    # para compara com uma nova analise (nova img) de entrada
+
+    # ANOTAÇÕES PARA CORRIGIR POSSIVELMENTE COM MACHINE LEARNING E ANALISE DE DADOS COM TESTES AUTOMATIZADOS DE ENTREVISTAS COM TRADUÇÃO PARA LIBRAS E TRANSCRIÇÕES 
+
+    # Não foram usadas as letras: H, J, K, X, ,Y , Z devido ao movimento adicional para a execução correta das letras.
+
+    # Estas letras podem ser analisadas em uma função diferente, semelhante a função de análise de posicionamento do corpo, onde comparamos a transição entre pontos
+
+    # Letra T: para o dedo polegar, devido a estar sobreposto pelo dedo indicador, o algoritmo não reconhece os pontos da ponta do dedo
+
+    # Letra N e U se confundem
+    arquivo_proto = "./modulos/pose_deploy.prototxt"
+    arquivo_pesos = "./modulos/pose_iter_102000.caffemodel"
+    numero_pontos = 22
+    pares_poses = [[0, 1], [1, 2], [2, 3], [3, 4], [0, 5], [5, 6], [6, 7], [7, 8], 
+                [0, 9], [9, 10], [10, 11], [11, 12], [0, 13], [13, 14], [14, 15],
+                [15, 16], [0, 17], [17, 18], [18, 19], [19, 20]]
+    
+    # AQUI PODEMOS IMPLEMENTAR MAIS LETRAS OU EASTER EGGS DE SIMBOLOS UNIVERSAIS
+    letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'I', 'L', 'M', 'N', 'O', 'P', 'Q',
+          'R', 'S', 'T', 'U', 'V', 'W']
+
+    # AQUI ELE CARREGA O MODELO QUE VAI FAZER O HAND TRACK DA MAO E ASSIM PREENCHER OS PARES DE PONTOS
+    modelo = cv2.dnn.readNetFromCaffe(arquivo_proto, arquivo_pesos)
+
+
+    #provavel q precise salvar a img para usar aqui****
+    imagem_copia = np.copy(img)
+
+    # DIMENSIONA
+    dimensions = img.shape
+    height = dimensions[0]
+    width = dimensions[1]
+    imagem_largura = width
+    imagem_altura = height
+    proporcao = imagem_largura / imagem_altura
+
+    # CONFIG DAS CORES DOS PONTOS, TAMANHO DAS LINHAS E FONTE, APENAS DETALHE
+    cor_pontoA, cor_pontoB, cor_linha = (14, 201, 255), (255, 0, 128), (192, 192, 192)
+    cor_txtponto = (10, 216, 245)
+    tamanho_fonte, tamanho_linha, tamanho_circulo, espessura = 5, 1, 4, 2
+    fonte = cv2.FONT_HERSHEY_SIMPLEX
+
+    # DIMENSIONA ENTRADA
+    entrada_altura = 256
+    entrada_largura = int(((proporcao * entrada_altura) * 8) // 8)
+
+    # AQUI COMEÇA A USAR EFETIVAMENTE O CV2
+    # CONVERTE IMG DO FORMATO OPENCV PARA O BLOBCAFFE Q É O TIPO DO MODELO QUE ESTAMOS USANDO
+    entrada_blob = cv2.dnn.blobFromImage(img, 1.0 / 255, 
+                                     (entrada_largura, entrada_altura), 
+                                     (0, 0, 0), swapRB=False, crop=False)
+    
+    # SAIDA DOS DADOS
+    modelo.setInput(entrada_blob)
+    saida = modelo.forward()
+
+    # APARTIR DESSE MOMENTO VAMOS COLOCAR ESSES DADOS NA IMAGEM ENTAO PODE COMENTAR NO FUTURO
+    pontos = []
+    limite = 0.1
+    for i in range(numero_pontos):
+        mapa_confianca = saida[0, i, :, :]
+        mapa_confianca = cv2.resize(mapa_confianca, (imagem_largura, imagem_altura))
+
+        _, confianca, _, ponto = cv2.minMaxLoc(mapa_confianca)
+
+        if confianca > limite:
+            cv2.circle(imagem_copia, (int(ponto[0]), int(ponto[1])), 5, cor_pontoA, 
+                    thickness=espessura, lineType=cv2.FILLED)
+            cv2.putText(imagem_copia, ' ' + (str(int(ponto[0]))) + ',' + 
+                        str(int(ponto[1])), (int(ponto[0]), int(ponto[1])),
+                        fonte, 0.3, cor_txtponto, 0, lineType=cv2.LINE_AA)
+
+            cv2.circle(img, (int(ponto[0]), int(ponto[1])), tamanho_circulo,
+                    cor_pontoA,
+                    thickness=espessura, lineType=cv2.FILLED)
+            cv2.putText(img, ' ' + "{}".format(i), (int(ponto[0]), 
+                                                    int(ponto[1])), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.3,
+                        cor_txtponto,
+                        0, lineType=cv2.LINE_AA)
+
+            pontos.append((int(ponto[0]), int(ponto[1])))
+
+        else:
+            pontos.append((0, 0))
+    check = False
+    ## DESENHA O ESQUELETO TENDO OS PONTO CHAVE APENAS JUNTAMOS OS PARES
+    for par in pares_poses:
+        parteA = par[0]
+        parteB = par[1]
+
+        if pontos[parteA] != (0,0) and pontos[parteB] != (0,0):
+            cv2.line(imagem_copia, pontos[parteA], pontos[parteB], cor_linha, 
+                 tamanho_linha, lineType=cv2.LINE_AA)
+            cv2.line(img, pontos[parteA], pontos[parteB], cor_linha, tamanho_linha, 
+                    lineType=cv2.LINE_AA)
+            check = True
+    
+    cv2.imwrite('testeHT.png', img)
+    if(check):
+        posicao.posicoes = []
+        # Dedo polegar
+        # print('------POSICOES DOS DEDOS------')
+        polegarM = posicao.verificar_posicao_DEDOS(pontos[1:5], 'polegar', altura.verificar_altura_MAO(pontos))
+        # Dedo indicador
+        indicadorM = posicao.verificar_posicao_DEDOS(pontos[5:9], 'indicador', altura.verificar_altura_MAO(pontos))
+        # Dedo médio
+        medioM = posicao.verificar_posicao_DEDOS(pontos[9:13], 'medio', altura.verificar_altura_MAO(pontos))
+        # Dedo anelar
+        anelarM = posicao.verificar_posicao_DEDOS(pontos[13:17], 'anelar', altura.verificar_altura_MAO(pontos))
+        # Dedo mínimo
+        minimoM = posicao.verificar_posicao_DEDOS(pontos[17:21], 'minimo', altura.verificar_altura_MAO(pontos))
+        # print('------POSICOES DOS DEDOS------')
+        # print('polegar: ', polegarM)
+        # print('indicadorM: ', indicadorM)
+        # print('medio: ', medioM)
+        # print('anelar: ', anelarM)
+        # print('minimo: ', minimoM)
+        
+        atual = proximidade.verificar_proximidade_DEDOS(pontos)
+        print(atual)
+        # return atual por enquanto por causa do mobile sla oq ta dando aqui 
+        for i, a in enumerate(alfabeto.letras):
+            if(alfabeto.letras[i] == atual):
+                # print('I(index do alfabeto):')
+                # print(i)
+                # print('A(descricao do gesto):')
+                # print(a)
+                # print('Deu boa!!!!!!!!')
+                final = []
+                final.append(a)
+                # print('ok')
+                # print(a)
+                final.append(i)
+                # print('return é [a,i]')
+        # print(final[0])
+        # print(final[1])
+        return atual
+    else:
+        return 'Nao foi possível identificar a mao!'
+
+
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Sized
 from flask.wrappers import Request, Response
+import numpy as np
 from werkzeug.utils import redirect, secure_filename
 from flask import Flask, request, flash,jsonify
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
@@ -20,6 +21,8 @@ import io
 import HT
 import time
 import json
+import asyncio
+from modulos import alfabeto 
 
 
 ## Start Stuffs
@@ -29,54 +32,82 @@ api = Api(app)
 # db = SQLAlchemy(app)
 ## Start Stuffs
 
-class HelloWorld(Resource):
-    def get(self):
-        # handtracked()
-        print('deu boa')
-        return {"data" : "Hello World"}
+# class HelloWorld(Resource):
+#     def get(self):
+#         handtracked()
+#         print('deu boa')
+#         return {"data" : "Hello World"}
 
-class HandTracked(Resource):
-    def get(self):
-        return handtracked()
+# class HandTracked(Resource):
+#     def get(self):
+#         return handtracked()
 
-class ImgPost(Resource):
-    def post(self):
+# class ImgPost(Resource):
+#     def post(self):
 
-        file = request.files['image']
-        img = Image.open(file.stream)
-        handtracked(img)
-        return jsonify({'msg':'sucess','size':[img.width, img.height]}) 
+#         file = request.files['image']
+#         img = Image.open(file.stream)
+#         handtracked(img)
+#         return jsonify({'msg':'sucess','size':[img.width, img.height]}) 
+
+async def teste():
+    ini = time.time()
+    print('inicio do metodo:')
+    print(ini)
+    #Aqui precisa mt rodar um metodo pegando essa img entao vamos la
+    file = request.files['imgData']
+    img = Image.open(file.stream)
+    nome = 'teste.png'
+    img.save(nome)
+    #Aqui temos um metodo para selecionar a img e processar os pontos chave das mãos, deixa salvo a img com os pontos
+    #Logo em seguida processa os pontos chave da mão para fazer uma leitura de como ela está
+    #Porfim passa pelo array de array do alfabeto assim concluindo o metodo
+    #Caso tudo ocorra bem o retorno é a letra do alfabeto correspondente ao gest
+    #Por enquanto retornos de teste 
+    statusCode  =  HT.srcPicture('./' + nome)
+    fin = time.time()
+    print('final do  metodo:')
+    print(fin)
+    temp = fin - ini
+    print('tempo de execução:')
+    print(temp)
+    print('statusCode: ')
+    letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'I', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W']
+    print(letras[statusCode])
+    return {'data':letras[statusCode]}
 
 @app.route('/post', methods = ['POST'])
-def upload_file():
-   ini = time.time()
-   print('inicio do metodo:')
-   print(ini)
-   #Aqui precisa mt rodar um metodo pegando essa img entao vamos la
-   file = request.files['imgData']
-   img = Image.open(file.stream)
-   nome = 'teste.png'
-   img.save(nome)
-   #Aqui temos um metodo para selecionar a img e processar os pontos chave das mãos, deixa salvo a img com os pontos
-   #Logo em seguida processa os pontos chave da mão para fazer uma leitura de como ela está
-   #Porfim passa pelo array de array do alfabeto assim concluindo o metodo
-   #Caso tudo ocorra bem o retorno é a letra do alfabeto correspondente ao gest
-   #Por enquanto retornos de teste 
-   statusCode = HT.srcPicture('./' + nome)
-   fin = time.time()
-   print('final do  metodo:')
-   print(fin)
-   temp = fin - ini
-   print('tempo de execução:')
-   print(temp)
-   print('statusCode: ')
-   letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'I', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W']
-   print(letras[statusCode])
-   return {'data':letras[statusCode]}
+async def upload_file():
+    ini = time.time()
+    print('inicio do metodo:')
+    print(ini)
+    #Aqui precisa mt rodar um metodo pegando essa img entao vamos la
+    file = request.files['imgData']
+    img = Image.open(file.stream)
+    nome = 'teste.jpg'
+    img.save(nome)
+    #Aqui temos um metodo para selecionar a img e processar os pontos chave das mãos, deixa salvo a img com os pontos
+    #Logo em seguida processa os pontos chave da mão para fazer uma leitura de como ela está
+    #Porfim passa pelo array de array do alfabeto assim concluindo o metodo
+    #Caso tudo ocorra bem o retorno é a letra do alfabeto correspondente ao gest
+    #Por enquanto retornos de teste 
+    
+    statusCode = await HT.final('./' + nome)
+    fin = time.time()
+    print('final do  metodo:')
+    print(fin)
+    temp = fin - ini
+    print('tempo de execução:')
+    print(temp)
+    print('statusCode: ')
+    letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'I', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W']
+    print(statusCode)
+    return {'data':statusCode}
 
-#method, request route
-api.add_resource(HelloWorld, "/helloworld")
-api.add_resource(HandTracked, "/hts")
+
+#method, request route [END POINT]
+# api.add_resource(HelloWorld, "/helloworld")
+# api.add_resource(HandTracked, "/hts")
 # api.add_resource(ImgPost, "/post")
 
 
